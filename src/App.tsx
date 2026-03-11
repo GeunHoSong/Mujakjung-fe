b54d8d0e039c02ac4f18af403d738c42368e44bb
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2026-03-11 21:23:41 +0900

    search.tsx 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,53 @@
+
+// react-router-dom에서 필요한 기능들을 import
+// BrowserRouter : 전체 앱에서 라우팅 기능을 활성화
+// Routes : 여러 Route를 묶어주는 컨테이너
+// Route : URL 경로에 따라 어떤 컴포넌트를 보여줄지 설정
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// 각 페이지 컴포넌트 import
+// Main : 메인 페이지
+// Join : 회원가입 페이지
+// Login : 로그인 페이지
 import Main from "./pages/Main";
 import Join from "./pages/Join";
 import Login from "./pages/Login";
 
 function App() {
   return (
+
+    // BrowserRouter
+    // React 앱에서 URL을 기반으로 페이지를 이동할 수 있게 해주는 라우터
+    // 보통 App 전체를 감싸서 사용
     <BrowserRouter>
+
+      {/* Routes
+          여러 Route들을 감싸는 컨테이너
+          내부에서 URL 경로에 맞는 Route 하나만 렌더링됨
+      */}
       <Routes>
+
+        {/* 
+          Route
+          path : URL 주소
+          element : 해당 주소로 들어왔을 때 보여줄 컴포넌트
+        */}
+
+        {/* "/" -> 사이트 첫 화면 (메인 페이지) */}
         <Route path="/" element={<Main />} />
+
+        {/* "/join" -> 회원가입 페이지 */}
         <Route path="/join" element={<Join />} />
+
+        {/* "/login" -> 로그인 페이지 */}
         <Route path="/login" element={<Login />} />
+        <Route path="/seach" element={<search/>}/>
+
       </Routes>
+
     </BrowserRouter>
   );
 }
 
+// 다른 파일에서 사용할 수 있도록 App 컴포넌트 export
 export default App;
---
+++ src/pages/Seach.tsx
@@ -0,0 +1,24 @@
+// react-router-dom 에서 url query 값을 가져오는 쪽 
+import { useSearchParams } from "react-router-dom";
+
+function Seach (){
+// url의 query parameter 가져 오기
+// 예: /search?keyword=${재주}&type=domstic
+const [params] = useSearchParams();
+
+// 검색어 가져오기 
+// url /keyword 값 추출 
+const keyword = params.get("keyword");
+// 여행 타입 가지고 오기 
+const type = params.get("type");
+return (
+    <div>
+        {/** 패이지 재목 */}
+        <h1>추천 여행 결과</h1>
+        {/** 검색 조건 확인 */}
+        <p>검색어: {keyword}</p>
+        <p>여행 타입: {type}</p>
+    </div>
+);
+}
+export default Seach;
