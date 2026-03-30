b84787f2d9d3337cd4bac4f50ac559cd1e4b070f
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2026-03-30 19:23:33 +0900

    카카오 주소 api 사용 하는중

---
+++ src/pages/member/Signup.js
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+import DaumPostcode from "react-daum-postcode";
+
+
+const Sipnup= ()=> {
+    // 기존 회원 가입 정보 (아이디, 비번)
+    const [email, setEmail] = useState('');
+    const [password, setPassword ] = useState ('');
+    // 주소 관련 상태 (아까 만들 거 )
+    const [zipcode, setZipcode] = useState('');
+    const [address, setAddress] = useState('');
+    const [detailAddress, setDetailAddress] = useState('');
+    const [isPostcodeOpen, setIsPostcodeOpen] = useState(false);
+    // 주소 선택 완료 핸들러 
+    const handleComplete = (data) =>{
+        setZipcode(data.zonecode);
+        setAddress(data.address);
+        setIsPostcodeOpen(data.isPostcodeOpen);
+    };
+    return (
+        <>
+        </>
+    )
+}
+export default Sipnup;
