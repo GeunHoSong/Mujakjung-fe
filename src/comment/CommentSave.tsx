e207d5efb26f809fdc23f5728797e11a5f00bef2
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2026-06-23 19:18:52 +0900

    댓글 작성 프로트 엔드만 만듬

--- src/comment/CommentSave.tsx
+++ src/comment/CommentSave.tsx
@@ -1,9 +1,31 @@
 import { useState } from "react";
 
-function CommentSave(){
+interface CommentSaveProps {
+    boardId: string | number;
+}
+
+function CommentSave({boardId}: CommentSaveProps){
+    const [comment , setComment] = useState("");
+    const [isLoading , setIsLoading] = useState(false);
+
+    const handleSave = async() => {
+        setIsLoading(true);
+
+        const commentData ={
+            boardId: boardId,
+            content: comment,
+
+        }
+
+        console.log("저장 한 댓글" , commentData);
+        setTimeout(()=> setIsLoading(false) , 100);
+    };
+
+    if(isLoading) return <div>로딩중  .,....</div>
     return (
         <div>
-            <p>로딩중 .....</p>
+            <textarea value={comment} onChange={(e) => setComment(e.target.value)} placeholder="댓글을 작성을 하세요"></textarea>
+            <button onClick={handleSave}>저장</button>
         </div>
     )
 }
