e25d0a72b4fca648c9a7ac6ddbc578b864e561b8
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2026-06-30 15:38:05 +0900

    cartList 만드는 중

--- src/pages/cart/CartList.tsx
+++ src/pages/cart/CartList.tsx
@@ -1,6 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+interface CartItem{
+    id: number;
+    name: string;
+    price: number;
+}
 
 function CartList(){
+    const [item, setItem ]= useState<CartItem[]>([]) 
+    useEffect(()=>{
+        // localstorage에서 데이터 불러 오는 중 
+        const saveCart = localStorage.getItem("cart");
+        if(saveCart) {
+            setItem(JSON.parse(saveCart));
+        }
+    }, [])
     return (
         <div>
             <p>리스트 불러 오는 중 .....</p>
