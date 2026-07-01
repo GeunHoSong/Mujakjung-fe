d9370b2d15589f86224414d3cffaae2b01b91137
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2026-07-01 19:21:51 +0900

    mypage 도매인

---
+++ src/pages/mypage/MyPageTravel.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function MyPageTravel(){
+    return (
+        <div>
+            <p>여행 일지 불러 오는 중 </p>
+        </div>
+    )
+}
+
+export default MyPageTravel;
