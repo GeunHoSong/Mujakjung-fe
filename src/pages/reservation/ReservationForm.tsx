1a6d914db1b85cb44cb2b74aa2e37a84a70102bf
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2026-07-25 19:55:51 +0900

    예약 시스템 만드는 중

--- src/pages/reservation/ReservationForm.tsx
+++ src/pages/reservation/ReservationForm.tsx
@@ -1,23 +1,42 @@
+
 import { useState } from "react";
 import Calendar from "react-calendar";
 import 'react-calendar/dist/Calendar.css';
 function ReservationForm(){
-    const [data , setData] = useState<Data>(new Data());
+    const [date , setDate] = useState<Date>(new Date());
 
     const addReservation = () => {
         // 날짜 함수를 형식을 문자열로 변환 
-        const formData = data.toISOString().split
+        const formattedDate = date.toISOString().split('T')[0];
+
+        const newReservation = {
+            id: Date.now(),
+            productName: "강릉 커피 투어",
+            departureData: formattedDate,
+            status: "예약 종료 "
+        };
+        const saveReserations = localStorage.getItem("reseration");
+        const reseration= saveReserations? JSON.parse(saveReserations) : [];
+
+        reseration.push(newReservation);
+        localStorage.setItem("reseration" , JSON.stringify(reseration));
+        alert(`${formattedDate}예약이 완료 되어 습니다` );
+
     }
-
-
-
-
-
-
-
-
     return (
-        <div>
+        <div style={{ padding: '20px'}}>
+            <h1>예약 하기 </h1>
+            <p>출발 날짜를 선택을 해주세요</p>
+            <div style={{marginBottom:'20px'}}>
+                <Calendar onChange={(value)=> setDate(value as Date)} value={date}/>
+            </div>
+            <div>
+                <p>선택 날짜: <strong>{date.toISOString().split('T')[0]}</strong></p>
+                <button 
+                onClick={addReservation}
+                style={{ padding: '10px 20px', backgroundColor: '#4CAF50', color: 'white', border: 'none', cursor: 'pointer' }}>예약 하기</button>
+            </div>
+
 
         </div>
     )
