39b8bf6984dec7c832f8a8eec33b488d84f0c607
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2026-07-26 19:26:04 +0900

    예약 목록

--- src/pages/reservation/ReservationList.tsx
+++ src/pages/reservation/ReservationList.tsx
@@ -1,26 +1,33 @@
-import { useState, useEffect } from "react";
-interface CartItem{
-    id: number;
-    name: string;
-    price: number;
-}
+import { useState } from "react";
 
-function CartList(){
-    const [item, setItem ]= useState<CartItem[]>([]) 
-    useEffect(()=>{
-        // localstorage에서 데이터 불러 오는 중 
-        const saveCart = localStorage.getItem("cart");
-        if(saveCart) {
-            setItem(JSON.parse(saveCart));
-        }
-    }, [])
+function ReservationList({reservation ,onDelete}){
+
     return (
         <div>
-            <p>리스트 불러 오는 중 .....</p>
+            <h2>나의 예약 시스템</h2>
+            {reservation.length === 0 ? (
+                <p style={{color: '#777', marginTop: '15px'}}>예약된 내용이 없습니다</p>
+            ):(
+                <ul style={{listStyle: 'none', padding: 0 , marginTop: '15px' }}>
+                    {reservation.map((item)=>(
+                        <li key={item.id} style={{marginBottom: '15px', padding: '15px', borderRadius: '8px', backgroundColor: '#f9f9f9', display: 'flex'
+                            , justifyContent: 'space-between', alignItems: 'center', boxShadow: '0 1px 3px rgba(0,0,0,0,0.25)'}}>\
+                            <div>
+                                <p style={{margin: '3px 0'}}><strong>상품명</strong>{item.productName}</p>
+                                <p style={{margin: '3px 0'}}><strong>출발 날짜</strong>{item.departureData}</p>
+                                <p style={{margin: '3px 0'}}><strong>상태</strong>{item.status}</p>
+
+
+
+                                </div>
+                            </li>
+
+                   ))}
+
+                </ul>
+            )}
         </div>
     )
-
-
 }
 
-export default CartList;
+export default ReservationList;
